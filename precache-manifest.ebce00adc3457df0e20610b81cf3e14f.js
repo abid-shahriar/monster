5e6d00412d88f056cfa81158d9f6bf7069f2bbb4
abid7 <abidshahriar7@gmail.com>
2020-05-05 23:48:27 +0600

    blah

---
+++ precache-manifest.ebce00adc3457df0e20610b81cf3e14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168a88484404f7bd228954c6674aebb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c957eca36a6d85b345a",
+    "url": "/static/css/main.59943b69.chunk.css"
+  },
+  {
+    "revision": "49a4d5797d6c3ec6d2db",
+    "url": "/static/js/2.95eaf400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95eaf400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c957eca36a6d85b345a",
+    "url": "/static/js/main.5db87f05.chunk.js"
+  },
+  {
+    "revision": "0308a4e40ae10ecbf9ad",
+    "url": "/static/js/runtime-main.0da00448.js"
+  }
+]);
